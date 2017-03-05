844e8817ff6f3c767fb87cd605a598dd48ec3d61
mav <>
2017-03-05 02:45:49 +0000

    Read/Write fix

--- server.c
+++ server.c
@@ -4,6 +4,7 @@
 #include <sys/socket.h>
 #include <netdb.h>
 #include <strings.h>
+#include <string.h>
 
 #define MAXLINE 256
 typedef struct sockaddr SA; 
@@ -13,7 +14,7 @@ struct sockaddr_in serverAddr, clientAddr;
 int main(int argc, char *argv[]){
 	
 	int listenerFD, newSocketFD, portNO, clientLen;			
-	int count;
+	int count, optval, bufferSize;
 	char buffer[256];
 	
 	if (argc < 2){
@@ -24,7 +25,9 @@ int main(int argc, char *argv[]){
 	listenerFD = socket(AF_INET, SOCK_STREAM, 0);
 	if (listenerFD < 0)
 		perror("Error, could not open socket...\n");
-	
+	if (setsockopt(listenerFD, SOL_SOCKET, SO_REUSEADDR, (const void*)&optval, sizeof(int))<0)
+		return -1;
+
 	bzero((char *)&serverAddr, sizeof(serverAddr));
 	portNO = atoi(argv[1]);
 	serverAddr.sin_family = AF_INET;
@@ -42,27 +45,44 @@ int main(int argc, char *argv[]){
 	while(1){
 		
 		newSocketFD = accept(listenerFD, (SA*)&clientAddr, &clientLen);
-		if (newSocketFD < 0)
-			perror("Error accepting...\n");
-		
-
-	printf("Listening on port %d\n", portNO);
-
-
-
-
-
+		if (newSocketFD < 0){
+			printf("Error accepting...\n");
+			continue;
+		}
+		else
+			printf("Client Accepted...\n");
 
 		bzero(buffer, MAXLINE);
-		count = read((newSocketFD, buffer, MAXLINE-1));
-		if (count < 0)
-			perror("Error reading from client...\n");
-	
-		printf("Message Received: %s...\n", buffer);
-	
-		count = write(newSocketFD, "Data received ...", 24);
-		if (count < 0)
-			perror("Error writing to client...\n");
+		printf("newsocketFD: %d, listenerFD: %d \n", newSocketFD, listenerFD);
+		count = read(newSocketFD, (char*)&bufferSize ,sizeof(bufferSize));
+		if (count < 0){
+			printf("Error reading %s from client, count: %d...\n", buffer, count);
+			continue;
+		}
+		else
+			printf("BufferSize Received: %d...\n", bufferSize);
+
+		count = read(newSocketFD, buffer, bufferSize);
+		if (count < 0){
+			printf("Error reading buffer\n");
+			continue;
+		}
+		else
+			printf("Buffer Received: %s...\n", buffer);
+
+		count = write(newSocketFD, (char*)&bufferSize, sizeof(bufferSize));
+		if (count < 0){
+			printf("Error writing to client...\n");
+			continue;
+		}
+		else
+			printf("Message written to client\n");
+		count = write(newSocketFD, buffer, bufferSize);
+		if (count < 0){
+			printf("Error writing 2 to client...\n");
+		}
+		else
+			printf("Message written 2 to client...\n");
 	}
 
 	return 0;
